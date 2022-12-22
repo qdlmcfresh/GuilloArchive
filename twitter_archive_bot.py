145c88d4f7f9e3a80e2a18457f9fa4e3d1d6f711
Philipp Urlbauer <philipp.urlbauer@hs-augsburg.de>
2022-12-22 13:25:06 +0100

    Use template strings with substitution

--- twitter_archive_bot.py
+++ twitter_archive_bot.py
@@ -1,4 +1,4 @@
-# Archive the twitter account https://twitter.com/GuillotineTea 
+# Archive the twitter account https://twitter.com/GuillotineTea
 # The Achive includes tweets, images, videos and gifs
 # The archive is stored in a sqlite database, and the images and videos are stored in a directory
 
@@ -7,6 +7,7 @@ import os
 import sqlite3
 import urllib.request
 import argparse
+from string import Template
 
 
 class TwitterArchiveBot:
@@ -23,11 +24,9 @@ class TwitterArchiveBot:
         self.db_cursor = self.conn.cursor()
         if(create):
             self.create_database()
-        
-
 
     def create_database(self):
-        
+
         print("Creating database file")
         c = self.db_cursor
         c.execute('''CREATE TABLE tweets
@@ -37,7 +36,6 @@ class TwitterArchiveBot:
                         (id integer primary key autoincrement, media_id text, tweet_id integer, media_url text, media_type text, FOREIGN KEY(tweet_id) REFERENCES tweets(tweet_id))''')
         self.conn.commit()
 
-    
     def save_tweet(self, tweet, media_dict):
         # save the tweet to the database
         # save the media to the media directory with the media_id as the filename
@@ -53,7 +51,8 @@ class TwitterArchiveBot:
                 url = media.url
                 if media.type == "video":
                     url = media.variants[-1]["url"]
-                c.execute("INSERT INTO media VALUES (NULL,?,?,?,?)", (media.media_key, tweet.id, url, media.type))
+                c.execute("INSERT INTO media VALUES (NULL,?,?,?,?)",
+                          (media.media_key, tweet.id, url, media.type))
         self.conn.commit()
 
     def download_media(self):
@@ -73,22 +72,24 @@ class TwitterArchiveBot:
             if media_type == "photo":
                 # download the image
                 print("Downloading image")
-                urllib.request.urlretrieve(url, self.media_path + "/" + key + ".jpg")
+                urllib.request.urlretrieve(
+                    url, self.media_path + "/" + key + ".jpg")
             elif media_type == "video":
                 # download the video
                 print("Downloading video")
-                urllib.request.urlretrieve(url, self.media_path + "/" + key + ".mp4")
+                urllib.request.urlretrieve(
+                    url, self.media_path + "/" + key + ".mp4")
             elif media_type == "animated_gif":
                 # download the gif
                 print("Downloading gif")
-                urllib.request.urlretrieve(url, self.media_path + "/" + key + ".gif")
-
+                urllib.request.urlretrieve(
+                    url, self.media_path + "/" + key + ".gif")
 
     def get_tweets(self, since_id=None):
         # get the tweets from the twitter account using tweepy
         client = tweepy.Client(bearer_token=self.token)
-        for results in tweepy.Paginator(client.get_users_tweets, id=self.id, since_id=since_id,  exclude='replies,retweets',expansions='attachments.media_keys', media_fields='type,media_key,url,variants', max_results=100):
-            #self.download_media(results.includes['media'])
+        for results in tweepy.Paginator(client.get_users_tweets, id=self.id, since_id=since_id,  exclude='replies,retweets', expansions='attachments.media_keys', media_fields='type,media_key,url,variants', max_results=100):
+            # self.download_media(results.includes['media'])
             if(results.data is None):
                 break
             print(f"Saving {len(results)} tweets")
@@ -97,97 +98,46 @@ class TwitterArchiveBot:
                 media_dict[media.media_key] = media
             for tweet in results.data:
                 self.save_tweet(tweet, media_dict)
-    
+
     def close(self):
         self.conn.close()
-    
-    
+
     def to_html(self):
+        post_template = None
+        html_template = None
+        with open('post_template.html') as f:
+            post_template = Template(f.read())
+        with open('template.html') as f:
+            html_template = Template(f.read())
+        posts = []
         tweets = self.db_cursor.execute("SELECT * FROM tweets").fetchall()
         # convert the database to html
-        post_footer = '''
-        </div>
-      </div>
-      <!-- post ends -->
-      '''
-        html = '''
-        <!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Guillo-Archiv</title>
-    <link rel="stylesheet" href="styles.css" />
-    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet" />
-    <link
-      rel="stylesheet"
-      href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"
-      integrity="sha512-iBBXm8fW90+nuLcSKlbmrPcLa0OT92xO1BIsZ+ywDWZCvqsWgccV3gFoRBv0z+8dLJgyAHIhR35VZc2oM/gI1w=="
-      crossorigin="anonymous"
-    />
-  </head>
-  
-      <!-- feed starts -->
-    <div class="feed">
-      <div class="feed__header">
-        <h2>Home</h2>
-      </div>
-        '''
         for tweet in tweets:
-            post_header = f'''
-              <!-- post starts -->
-      <div class="post">
-        <div class="post__avatar">
-          <img
-            src="https://pbs.twimg.com/profile_images/1356319145403494402/L2ZsJGIK_400x400.jpg"
-            alt=""
-          />
-        </div>
-
-        <div class="post__body">
-          <div class="post__header">
-            <div class="post__headerText">
-              <h3>
-                {"Guillotine Tea"}
-                <span class="post__headerSpecial"
-                  ><span class="material-icons post__badge"> verified </span>@GuillotineTea</span
-                >
-              </h3>
-            </div>
-            <div class="post__headerDescription">
-              <p>{tweet[1]}</p>
-            </div>
-          </div>
-        '''
-            html += post_header
-            media = self.db_cursor.execute("SELECT * FROM media WHERE tweet_id=?", (tweet[0],)).fetchall()
+            media_html = ""
+            media = self.db_cursor.execute(
+                "SELECT * FROM media WHERE tweet_id=?", (tweet[0],)).fetchall()
             for file in media:
                 key = file[1]
                 url = file[3]
                 media_type = file[4]
                 if media_type == "photo":
-                    html += f"<img src='{self.media_path}/{key}.jpg' alt='image'><br>"
+                    media_html += f"<img src='{self.media_path}/{key}.jpg' alt='image'><br>"
                 elif media_type == "video":
-                    html += f"<video src='{self.media_path}/{key}.mp4' alt='video'></video><br>"
+                    media_html += f"<video src='{self.media_path}/{key}.mp4' alt='video' controls></video><br>"
                 elif media_type == "animated_gif":
-                    html += f"<img src='{self.media_path}/{key}.gif' alt='gif'><br>"
-            html += post_footer
-        html += '''
-            </div>
-    <!-- feed ends -->
-        </body>
-        </html>
-        '''
-        with open("index.html", "w") as f:
-            f.write(html)
+                    media_html += f"<img src='{self.media_path}/{key}.gif' alt='gif'><br>"
+            posts.append(post_template.substitute(
+                TEMPLATE_POST_TEXT=tweet[1], TEMPLATE_POST_MEDIA=media_html))
+        with open("index.html", "w", encoding='utf-8') as f:
+            f.write(html_template.substitute(TEMPLATE_POSTS=''.join(posts)))
 
     def to_markdown(self):
         # convert the database to markdown
         tweets = self.db_cursor.execute("SELECT * FROM tweets").fetchall()
         for tweet in tweets:
             print(tweet[1])
-            media = self.db_cursor.execute("SELECT * FROM media WHERE tweet_id=?", (tweet[0],)).fetchall()
+            media = self.db_cursor.execute(
+                "SELECT * FROM media WHERE tweet_id=?", (tweet[0],)).fetchall()
             for file in media:
                 key = file[1]
                 url = file[3]
@@ -200,9 +150,11 @@ class TwitterArchiveBot:
                     print(f"![]({self.media_path}/{key}.gif)")
             print("  ")
 
+
 if __name__ == "__main__":
     argparser = argparse.ArgumentParser()
-    argparser.add_argument("-b", "--bearer_token", help="Bearer Token", required=True, type=str)
+    argparser.add_argument("-b", "--bearer_token",
+                           help="Bearer Token", required=True, type=str)
     argparser.add_argument("-i", "--id", help="Twitter ID", required=True)
     argparser.add_argument("-d", "--db", help="Database File", required=True)
     args = argparser.parse_args()
@@ -210,15 +162,11 @@ if __name__ == "__main__":
     twitter_id = args.id
     db_file = args.db
     archive_bot = TwitterArchiveBot(twitter_id, bearer_token, db_file)
-    latest_tweet = archive_bot.db_cursor.execute("SELECT tweet_id FROM tweets ORDER BY tweet_id DESC LIMIT 1").fetchone()
+    latest_tweet = archive_bot.db_cursor.execute(
+        "SELECT tweet_id FROM tweets ORDER BY tweet_id DESC LIMIT 1").fetchone()
     if latest_tweet is not None:
         latest_tweet = latest_tweet[0]
     archive_bot.get_tweets(since_id=latest_tweet)
     archive_bot.download_media()
     archive_bot.to_html()
     archive_bot.close()
-
-    
-
-
-
